a835bcc7bfc1960bf6fd8c444c0d23511ffca5b2
Sofia Figueiredo de Oliveira <sofiafo804@gmail.com>
2026-05-23 18:14:39 -0300

    Add tipo Quarto

---
+++ src/main/java/com/puc/stayhub/model/TipoQuarto.java
@@ -0,0 +1,10 @@
+package com.puc.stayhub.model;
+
+/**
+ * Tipos de quarto disponiveis. Usado como discriminador na heranca JPA.
+ */
+public enum TipoQuarto {
+    INDIVIDUAL,
+    DUPLO,
+    FAMILIA
+}
